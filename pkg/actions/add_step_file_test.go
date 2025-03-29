06ddaee72f3fc9fa01a985c9af470bf8eac10e0a
Christoph Becker <post@christophb.de>
2025-03-29 12:25:06 +0100

    feat: add action to add new files to the migration config

--- pkg/actions/add_step_file_test.go
+++ pkg/actions/add_step_file_test.go
@@ -1,1 +1,65 @@
 package actions
+
+import (
+	"crypto/rand"
+	"github.com/stretchr/testify/suite"
+	"io"
+	"os"
+	"path"
+	"testing"
+)
+
+type AddStepFileTestSuite struct {
+	suite.Suite
+	actions                    *Actions
+	definitionReaderWriterMock *MigrationDefinitionReaderWriterMock
+	executionLoggerMock        *ExecutionLoggerMock
+}
+
+func (s *AddStepFileTestSuite) SetupTest() {
+	s.executionLoggerMock = NewExecutionLoggerMock(s.T())
+	s.definitionReaderWriterMock = NewMigrationDefinitionReaderWriterMock(s.T())
+	s.actions = New(s.executionLoggerMock, s.definitionReaderWriterMock)
+}
+
+func (s *AddStepFileTestSuite) TestAddStepFile() {
+	tmpFile, err := os.CreateTemp("", "test_file_*.sh")
+	s.Require().NoError(err)
+
+	_, err = io.WriteString(tmpFile, rand.Text())
+	s.Require().NoError(err)
+
+	folder, expectedFileName := path.Split(tmpFile.Name())
+
+	initialDefinition := MigrationDefinition{
+		Steps: []MigrationStep{
+			{
+				Filename:    rand.Text() + ".sh",
+				Description: rand.Text(),
+				Hash:        rand.Text(),
+			},
+		},
+	}
+
+	expectedDefinitionFileName := path.Join(folder, migrationFileName)
+
+	s.definitionReaderWriterMock.EXPECT().Read(expectedDefinitionFileName).Return(initialDefinition, nil)
+
+	expectedResultDefinition := MigrationDefinition{}
+
+	expectedHash, err := CalculateHash(tmpFile.Name(), initialDefinition.Steps[0].Hash)
+	s.NoError(err)
+	expectedResultDefinition.Steps = append(initialDefinition.Steps, MigrationStep{
+		Filename: expectedFileName,
+		Hash:     expectedHash,
+	})
+
+	s.definitionReaderWriterMock.EXPECT().Write(expectedDefinitionFileName, expectedResultDefinition).Return(nil)
+
+	err = s.actions.AddStepFile(folder, expectedFileName)
+	s.NoError(err)
+}
+
+func TestActions_AddStepFile(t *testing.T) {
+	suite.Run(t, new(AddStepFileTestSuite))
+}
