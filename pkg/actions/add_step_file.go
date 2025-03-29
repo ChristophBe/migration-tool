8cfa3e66ca2efe5e6fc760a895cfb795eccfe367
Christoph Becker <post@christophb.de>
2025-03-29 12:25:06 +0100

    feat: add action to add new files to the migration config

--- pkg/actions/add_step_file.go
+++ pkg/actions/add_step_file.go
@@ -1,5 +1,33 @@
 package actions
 
+import "path"
+
 func (a *Actions) AddStepFile(folder, filename string) error {
-	panic("implement me ")
+
+	definitionFile := path.Join(folder, migrationFileName)
+	migrationDefinition, err := a.definitionReaderWriter.Read(definitionFile)
+
+	if err != nil {
+		return err
+	}
+
+	lastHash := migrationDefinition.Steps[len(migrationDefinition.Steps)-1].Hash
+
+	newHash, err := CalculateHash(path.Join(folder, filename), lastHash)
+
+	if err != nil {
+		return err
+	}
+
+	migrationDefinition.Steps = append(migrationDefinition.Steps, MigrationStep{
+		Filename: filename,
+		Hash:     newHash,
+	})
+
+	err = a.definitionReaderWriter.Write(definitionFile, migrationDefinition)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
