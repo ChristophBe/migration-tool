d31b25b615126ea47f227d89ad0bddd3bca0635e
Christoph Becker <post@christophb.de>
2025-04-18 13:49:04 +0200

    feat: read regenerate-hashes command

---
+++ cmd/migration-tool/cmd/regenerateHashes.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2025 Christoph Becker <post@christopb.de>
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// regenerateHashesCmd represents the regenerateHashes command
+var regenerateHashesCmd = &cobra.Command{
+	Use:   "regenerate-hashes",
+	Short: "This command recalculates the hashes of the scripts.",
+	Long: `Regenerate hashes of the scripts. This is useful if the scripts have been intentional changed.
+Be careful, this can lead to consistent behavior while executing the scripts. It might prevent the run command to execute scripts in some cases.
+It is recommended that this is only used to recalculate the hashes for scripts that were not executed in any environment before.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		act := initActions(executionLogFile)
+
+		err := act.RecalculateHashes(baseFolder)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(regenerateHashesCmd)
+}
